19812b1052d166ef5e65f1b3e3b75335c616ce81
Gustavo Pinho <gustavo@nave.rs>
2019-01-26 14:27:27 -0200

    Fix redirect to user and code adjusts.

--- src/component/template/login.jsx
+++ src/component/template/login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Redirect, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const initialState = {
   user: { email: '', password: '' },
@@ -10,26 +10,17 @@ const initialState = {
 const baseURL = 'http://api-navetest.herokuapp.com/v1'
 
 export default class Login extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { ...initialState }
-
-    if (!window.localStorage.getItem('token')) {
-      window.localStorage.setItem('token', '')
-    }
-
-    this.login = this.login.bind(this)
-    this.updateField = this.updateField.bind(this)
-    this.returnLogin = this.returnLogin.bind(this)
+  state = {
+    ...initialState
   }
 
-  updateField (event) {
+  updateField = event => {
     const user = { ...this.state.user }
     user[event.target.name] = event.target.value
     this.setState({ user })
   }
 
-  login (e) {
+  login = e => {
     e.preventDefault()
     const data = JSON.stringify({ ...this.state.user })
     const config = {
@@ -39,35 +30,27 @@ export default class Login extends Component {
     }
     axios.post(`${baseURL}/users/login`, data, config).then(resp => {
       window.localStorage.setItem('token', resp.data.token)
-      this.setState({ ...this.state, token: resp.data.token, redirect: true })
+      const { history } = this.props
+      history.push('/users')
     })
   }
 
-  returnLogin () {
+  render () {
     return (
       <div className='container d-flex vw-100 vh-100 justify-content-center align-items-center'>
-        <form className='w-50'>
+        <form className='w-50' onSubmit={this.login}>
           <div className='form-group'>
             <input type='email' className='form-control' id='email' name='email'
-              placeholder='Email' onChange={e => this.updateField(e)} />
+              placeholder='Email' onChange={this.updateField} />
           </div>
           <div className='form-group'>
             <input type='password' className='form-control' id='password' name='password'
-              placeholder='Password' onChange={e => this.updateField(e)} />
+              placeholder='Password' onChange={this.updateField} />
           </div>
-          <button type='submit' className='btn btn-primary' onClick={this.login}>Entrar</button>
-          <Link to='/create'><button type='submit' className='btn btn-primary'>Cadastrar</button></Link>
+          <button type='submit' className='btn btn-primary'>Entrar</button>
+          <Link to='/create'><button type='button' className='btn btn-primary'>Cadastrar</button></Link>
         </form>
       </div>
-    )
-  }
-
-  render () {
-    if (this.state.redirect) {
-      return <Redirect to={`/users?token=${this.state.token}`} />
-    }
-    return (
-      this.returnLogin()
     )
   }
 }
